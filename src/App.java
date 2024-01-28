97f56e956331d68326ed4a7b2529b3d87dfd9877
Katerina Dorofeieva <dorofeievakatya20@gmail.com>
2024-01-28 13:51:40 +0200

    New branch for LR_1

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("I want to create new commit");
+        System.out.println("The new branch is merged with the main one");
     }
 }
